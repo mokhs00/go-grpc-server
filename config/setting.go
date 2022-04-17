30917b9cc3ee335f30fe9107f722a865e1ae09bf
mokhs00 <mokhs00@naver.com>
2022-04-17 19:36:04 +0900

    Add Setting

---
+++ config/setting.go
@@ -0,0 +1,23 @@
+package config
+
+import "go-grpc-server/util"
+
+type Setting struct {
+	Env        string
+	DBName     string
+	DBHost     string
+	DBPort     int
+	DBUser     string
+	DBPassword string
+}
+
+func NewSetting() Setting {
+	return Setting{
+		Env:        util.MustGetEnv("ENV", "development"),
+		DBName:     util.MustGetEnv("DB_NAME", "go-grpc-server"),
+		DBHost:     util.MustGetEnv("DB_HOST", "localhost"),
+		DBPort:     util.MustAtoi(util.MustGetEnv("DB_PORT", "3306")),
+		DBUser:     util.MustGetEnv("DB_USER", "some_user"),
+		DBPassword: util.MustGetEnv("DB_PASSWORD", "1234"),
+	}
+}
